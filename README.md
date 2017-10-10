d2e9a3bdedb9b3e8292c181164fb686cd0257c3a
Ruta Sakalauskaite <ruta.sakalauskaite@uk.tesco.com>
2017-10-10 10:46:23 +0300

    sudoku project

--- README.md
+++ README.md
@@ -3,11 +3,19 @@
 
 # Question 1 (Naked Twins)
 Q: How do we use constraint propagation to solve the naked twins problem?  
-A: *Student should provide answer here*
+A: The naked twins are unsolved boxes in a unit that have the same potential solutions, for example, '23' and '23'.
+In this particular example, since we know that '2' and '3' must be in one of these two boxes, we can remove these values
+from the rest of the unsolved boxes in the unit, simplifying the problem at hand.
+In my solution of the naked twins problem, I look for values in a unit that are of length 2 and are found more than once
+(the constraints). If any are found, I proceed to remove digits found in these values from the rest of the boxes in the
+unit.
 
 # Question 2 (Diagonal Sudoku)
 Q: How do we use constraint propagation to solve the diagonal sudoku problem?  
-A: *Student should provide answer here*
+A: Diagonal sudoku includes diagnals as units (not just rows, columns and square units). Introducing this adds one more
+set of constraints to the problem, narrowing down the number of possible solutions for each unsolved box.
+In my solution of the diagonal sudoku problem, I get the diagonals from zipping the rows' ids with columns' ids and
+rows' ids with reversed columns ids since the indices of the ids' positions correspond in both cases (the constraint).
 
 ### Install
 
@@ -24,21 +32,21 @@ If not, please see how to download pygame [here](http://www.pygame.org/download.
 
 ### Code
 
-* `solution.py` - Fill in the required functions in this file to complete the project.
-* `test_solution.py` - You can test your solution by running `python -m unittest`.
-* `PySudoku.py` - This is code for visualizing your solution.
-* `visualize.py` - This is code for visualizing your solution.
+* `solution.py` - You'll fill this in as part of your solution.
+* `solution_test.py` - Do not modify this. You can test your solution by running `python solution_test.py`.
+* `PySudoku.py` - Do not modify this. This is code for visualizing your solution.
+* `visualize.py` - Do not modify this. This is code for visualizing your solution.
 
 ### Visualizing
 
-To visualize your solution, please only assign values to the values_dict using the `assign_value` function provided in solution.py
+To visualize your solution, please only assign values to the values_dict using the ```assign_values``` function provided in solution.py
 
 ### Submission
 Before submitting your solution to a reviewer, you are required to submit your project to Udacity's Project Assistant, which will provide some initial feedback.  
 
 The setup is simple.  If you have not installed the client tool already, then you may do so with the command `pip install udacity-pa`.  
 
-To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login) for alternate login instructions.
+To submit your code to the project assistant, run `udacity submit` from within the top-level directory of this project.  You will be prompted for a username and password.  If you login using google or facebook, visit [this link](https://project-assistant.udacity.com/auth_tokens/jwt_login for alternate login instructions.
 
 This process will create a zipfile in your top-level directory named sudoku-<id>.zip.  This is the file that you should submit to the Udacity reviews system.
 
--- solution.py
+++ solution.py
@@ -1,4 +1,23 @@
 assignments = []
+rows = 'ABCDEFGHI'
+cols = '123456789'
+
+def cross(a, b):
+    "Cross product of elements in a and elements in b."
+    return [s+t for s in a for t in b]
+
+def diagonal_zip(a, b):
+    "Zip elements in a and b"
+    return [x+y for x, y in zip(a, b)]
+
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+diagonal_units = [diagonal_zip(rows, cols), diagonal_zip(rows, cols[::-1])]
+unitlist = row_units + column_units + square_units + diagonal_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
 def assign_value(values, box, value):
     """
@@ -23,13 +42,17 @@ def naked_twins(values):
     Returns:
         the values dictionary with the naked twins eliminated from peers.
     """
-
     # Find all instances of naked twins
-    # Eliminate the naked twins as possibilities for their peers
-
-def cross(A, B):
-    "Cross product of elements in A and elements in B."
-    pass
+    for unit in unitlist:
+        unit_values = [values[box] for box in unit]
+        twins = set(value for value in unit_values if (unit_values.count(value) > 1 and len(value) == 2))
+        # Eliminate the naked twins as possibilities for their peers
+        for twin in twins:
+            for digit in twin:
+                for box in unit:
+                    if digit in values[box] and values[box] != twin:
+                        values = assign_value(values, box, values[box].replace(digit, ''))
+    return values
 
 def grid_values(grid):
     """
@@ -41,7 +64,16 @@ def grid_values(grid):
             Keys: The boxes, e.g., 'A1'
             Values: The value in each box, e.g., '8'. If the box has no value, then the value will be '123456789'.
     """
-    pass
+    chars = []
+    digits = '123456789'
+    for c in grid:
+        if c in digits:
+            chars.append(c)
+        if c == '.':
+            chars.append(digits)
+    assert len(chars) == 81
+    return dict(zip(boxes, chars))
+
 
 def display(values):
     """
@@ -49,19 +81,59 @@ def display(values):
     Args:
         values(dict): The sudoku in dictionary form
     """
-    pass
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    return
 
 def eliminate(values):
-    pass
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit,'')
+    return values
 
 def only_choice(values):
-    pass
+     for unit in unitlist:
+         for digit in '123456789':
+             dplaces = [box for box in unit if digit in values[box]]
+             if len(dplaces) == 1:
+                 values[dplaces[0]] = digit
+     return values
+
 
 def reduce_puzzle(values):
-    pass
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = naked_twins(values)
+        values = only_choice(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
 
 def search(values):
-    pass
+    values = reduce_puzzle(values)
+    if values is False:
+        return False
+    if all(len(values[s]) == 1 for s in boxes):
+        return values
+    n,s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
 
 def solve(grid):
     """
@@ -72,6 +144,7 @@ def solve(grid):
     Returns:
         The dictionary representation of the final sudoku grid. False if no solution exists.
     """
+    return search(grid_values(grid))
 
 if __name__ == '__main__':
     diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
